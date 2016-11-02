580748c63a7a3336f5d220a094e532a3847e1787
root <root@o-l.pw>
2016-11-02 08:43:54 -0400

    add vector<char>2string something about stl

---
+++ c++/fstream/i/stringwithofstream.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<string>
+using std::cin;
+using std::cout;
+//using std::ofstream;
+using std::ios_base;
+using std::string;
+using std::vector;
+using namespace std;
+int main(int argc,char *argv[])
+{
+	string filename;
+	ofstream fout;
+	if(argc==1)
+	{	
+	cout<<"Enter name for new file: ";
+	cin>>filename;
+	
+        fout.open(filename.c_str());
+	//ofstream fout(filename.c_str(),ios_base::out|ios_base::app);
+	}
+	else 
+	{
+	filename=argv[1];	
+	//ofstream fout;
+	fout.open(filename.c_str());
+	//ofstream fout(argv[1],ios_base::out|ios_base::app);
+	}
+	vector <char> fn(filename.begin(),filename.end());
+	vector <char>::iterator arr=fn.begin();
+	while(arr!=fn.end())
+	{
+		fout<<*arr<<" ";
+		arr++;
+	}
+	fout.close();
+	return 0;
+}
